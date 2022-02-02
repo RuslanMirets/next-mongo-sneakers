5665a47ecf0924c6991c52a84db9aab92df5154d
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-02 17:57:27 +0300

    Fix Loading

--- components/Loading/index.jsx
+++ components/Loading/index.jsx
@@ -5,7 +5,7 @@ import styles from './Loading.module.scss';
 const Loading = () => {
   return (
     <div className={styles.loader}>
-      <CircularProgress color='inherit' />
+      <CircularProgress classes={{ root: styles.root }} color="inherit" />
     </div>
   );
 };
